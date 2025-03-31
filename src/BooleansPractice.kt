2ed72df28c968752c1460ce3ab5aa7be165e8dff
laura <laura.carolpavani@gmail.com>
2025-03-31 22:41:55 +1100

    Adding Boolean examples to practice

---
+++ src/BooleansPractice.kt
@@ -0,0 +1,10 @@
+package com.example.tutorialseu_arraylists
+
+fun main(){
+    var myTrue = true
+    var myFalse = false
+
+    println(myTrue || myFalse) //This will return true
+    println(myTrue && myFalse) //This will return false
+    println(!myTrue) //This will return false
+}
--- src/FloatingTypes.kt
+++ src/FloatingTypes.kt
@@ -1,15 +1,15 @@
 package com.example.tutorialseu_arraylists
 
 fun main(){
-    var circleRatio = 3.14 //First variable contain a Double so we can compare with the other variables below
+    var circleRatio = 3.14 //First variable contain a Double so we can compare with the other variables below = 3.14
     println(circleRatio)
-    var circleRatio2 = 3.14f //declaring as float number
+    var circleRatio2 = 3.14f //declaring as float number = 3.14
     println(circleRatio2)
     var circleRatio3 = 3.1415926535F //changing for a long float to check the message: floating-point literal cannot be
-                                    // represented with the required precision
+                                    // represented with the required precision = 3.1415927
     println(circleRatio3)
-    var circleRatio4 = 3.1415926535 //changing the number to double to check the print result
+    var circleRatio4 = 3.1415926535 //changing the number to double to check the print result = 3.1415926535
     println(circleRatio4)
-    var circleRatio5 = 3.1415926535897932 //assigning a longer value to compare the print result
+    var circleRatio5 = 3.1415926535897932 //assigning a longer value to compare the print result = 3.141592653589793
     println(circleRatio5)
 }
