0484fd7d1742b676c4b035800eb56e44ad297ddd
laura <laura.carolpavani@gmail.com>
2025-04-02 22:41:32 +1100

    While Loop example with user input

--- src/WhileLoop.kt
+++ src/WhileLoop.kt
@@ -1,11 +1,21 @@
 package com.example.tutorialseu_arraylists
-
+/*
 fun main(){
     var count = 0
     while (count <= 3){
         //so long this content will be executed
         println("Count is $count")
         count++
+    }
+    println("Loop is done!")
+}*/
+
+fun main(){
+    var userInput = readln()
+    while (userInput == "1"){
+        //so long this content will be executed
+        println("While loop executed")
+        userInput = readln()
     }
     println("Loop is done!")
 }
