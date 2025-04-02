cc59b3ea7052120a59d5d63841e4be7531d6b7a6
laura <laura.carolpavani@gmail.com>
2025-04-02 22:35:42 +1100

    While Loop example to save in my Kotlin material

---
+++ src/WhileLoop.kt
@@ -0,0 +1,11 @@
+package com.example.tutorialseu_arraylists
+
+fun main(){
+    var count = 0
+    while (count <= 3){
+        //so long this content will be executed
+        println("Count is $count")
+        count++
+    }
+    println("Loop is done!")
+}
